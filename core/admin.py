cd6a8ca4bb7719536d4fd4f0e37529c2700ca899
Filipe Medeiros <fcma@cin.ufpe.br>
2017-03-16 23:27:28 -0300

    Adicionando model e DjangoAdmin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Link 
+
+class LinkAdmin(admin.ModelAdmin):
+	list_display = ['url', 'slug']
+	search_fields = ['name', 'slug']
+
+
+admin.site.register(Link, LinkAdmin)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Link (models.Model):
+
+	url = models.CharField ('URL', max_length=500)
+	slug = models.SlugField ('Identificador', max_length=200)
+	
